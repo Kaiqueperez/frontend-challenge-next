6b90452a6c6d69e28bd4d8d005a659c0e2b7814f
Kaique Perez <kaique.perez@petzpartner.com.br>
2023-10-05 19:20:04 -0300

    feat-PP-001(global style): adding global style with styled components and install initial deps

---
+++ src/lib/registry/index.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useServerInsertedHTML } from "next/navigation";
+import React, { useState } from "react";
+import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+import * as S from "./styles";
+
+export default function StyledComponentsRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    styledComponentsStyleSheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== "undefined")
+    return (
+      <>
+        <S.GlobalStyle />
+        {children}
+      </>
+    );
+
+  return (
+    <>
+      <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+        {children}
+      </StyleSheetManager>
+    </>
+  );
+}
---
+++ src/lib/registry/styles.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+   @font-face {
+    font-family: 'Saira';
+    src: url('https://fonts.google.com/specimen/Saira');
+    
+   }
+
+   * {
+    font-family: 'Saira';
+   }
+`;
